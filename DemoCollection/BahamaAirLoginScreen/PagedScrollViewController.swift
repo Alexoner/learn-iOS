a253fc4660bbe3894da1273d535f282898192658
Alexoner <onerhao@gmail.com>
2016-01-25 14:08:20 +0800

    paging with UIScrollView: change the page on clicking the dots

---
+++ DemoCollection/BahamaAirLoginScreen/PagedScrollViewController.swift
@@ -0,0 +1,157 @@
+//
+//  PagedScrollViewController.swift
+//  BahamaAirLoginScreen
+//
+//  Created by duhao.dh on 1/25/16.
+//  Copyright © 2016 Razeware LLC. All rights reserved.
+//
+
+import UIKit
+
+class PagedScrollViewController: UIViewController {
+    
+    @IBOutlet var scrollView: UIScrollView!
+    @IBOutlet var pageControl: UIPageControl!
+    
+    var pageImages: [UIImage] = []
+    var pageViews: [UIImageView?] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        // 1
+        pageImages = [UIImage(named: "photo1.png")!,
+            UIImage(named: "photo2.png")!,
+            UIImage(named: "photo3.png")!,
+            UIImage(named: "photo4.png")!,
+            UIImage(named: "photo5.png")!]
+        
+        let pageCount = pageImages.count
+        
+        // 2
+        pageControl.currentPage = 0
+        pageControl.numberOfPages = pageCount
+        
+        // 3
+        for _ in 0..<pageCount {
+            pageViews.append(nil)
+        }
+        
+        // 4
+        let pagesScrollViewSize = scrollView.frame.size
+        scrollView.contentSize = CGSize(width: pagesScrollViewSize.width * CGFloat(pageImages.count),
+            height: pagesScrollViewSize.height)
+        
+        // 5
+        loadVisiblePages()
+    }
+    
+    func loadPage(page: Int) {
+        if page < 0 || page >= pageImages.count {
+            // If it's outside the range of what you have to display, then do nothing
+            return
+        }
+        
+        // 1
+        if let pageView = pageViews[page] {
+            // Do nothing. The view is already loaded.
+        } else {
+            // 2
+            var frame = scrollView.bounds
+            frame.origin.x = frame.size.width * CGFloat(page)
+            frame.origin.y = 0.0
+            
+            // 3
+            let newPageView = UIImageView(image: pageImages[page])
+            newPageView.contentMode = .ScaleAspectFit
+            newPageView.frame = frame
+            scrollView.addSubview(newPageView)
+            
+            // 4
+            pageViews[page] = newPageView
+        }
+    }
+    
+    func purgePage(page: Int) {
+        if page < 0 || page >= pageImages.count {
+            // If it's outside the range of what you have to display, then do nothing
+            return
+        }
+        
+        // Remove a page from the scroll view and reset the container array
+        if let pageView = pageViews[page] {
+            pageView.removeFromSuperview()
+            pageViews[page] = nil
+        }
+    }
+    
+    func loadVisiblePages(newPage: Int? = nil) {
+        // First, determine which page is currently visible
+        var page  = 0
+        if newPage == nil || newPage < 0 || newPage >= pageImages.count {
+            let pageWidth = scrollView.frame.size.width
+            page = Int(floor((scrollView.contentOffset.x * 2.0 + pageWidth) / (pageWidth * 2.0)))
+            // Update the page control
+            pageControl.currentPage = page
+        } else {
+            page = newPage!
+        }
+        print("page changed ", page)
+        
+        // Work out which pages you want to load
+        let firstPage = page - 1
+        let lastPage = page + 1
+        
+        // Purge anything before the first page
+        for var index = 0; index < firstPage; ++index {
+            purgePage(index)
+        }
+        
+        // Load pages in our range
+        for index in firstPage...lastPage {
+            loadPage(index)
+        }
+        
+        // Purge anything after the last page
+        for var index = lastPage+1; index < pageImages.count; ++index {
+            purgePage(index)
+        }
+    }
+    
+    func scrollViewDidScroll(scrollView: UIScrollView!) {
+        // Load the pages that are now on screen
+        loadVisiblePages()
+    }
+    
+    // MARK: page control delegate
+    
+    // MARK: page control action
+    @IBAction func changePage(sender: UIPageControl) {
+        let page = sender.currentPage
+        loadVisiblePages(page)
+        //let frame = scrollView.frame.size.width
+        var frame = scrollView.frame
+        frame.origin.x = scrollView.frame.size.width * CGFloat(page)
+        frame.origin.y = 0.0
+
+        scrollView.scrollRectToVisible(frame, animated: true)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
